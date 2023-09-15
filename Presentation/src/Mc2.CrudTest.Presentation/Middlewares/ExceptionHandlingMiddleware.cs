73da2678ffee85df868cbb246b007d0970181de6
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:24:05 +0330

    fedat: middlewar is added for catching exception

---
+++ Presentation/src/Mc2.CrudTest.Presentation/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace Mc2.CrudTest.Presentation.Server.Middlewares;
+
+public class ExceptionHandlingMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        var response = context.Response;
+        response.ContentType = "application/json";
+
+        var result = JsonConvert.SerializeObject(new
+        {
+            message = exception.Message,
+            isError = true,
+            detail = exception.StackTrace
+        });
+
+        await context.Response.WriteAsync(result);
+    }
+}
