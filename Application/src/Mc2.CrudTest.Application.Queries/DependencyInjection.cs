8ef3580eb243ec2a7332a38c4ce7294a6486fc9b
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:31:16 +0330

    feat: query project is created

---
+++ Application/src/Mc2.CrudTest.Application.Queries/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Mc2.CrudTest.Application.Queries.Customer.Get;
+using Mc2.CrudTest.framework.Mediator.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mc2.CrudTest.Application.Queries;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddQueryPipeline(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddTransient<IValidator<GetCustomerQuery>, GetCustomerQueryValidator>();
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(assembly)
+                .AddOpenBehavior(typeof(ValidationBehavior<,>))
+                .AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        return services;
+    }
+}
---
+++ Application/src/Mc2.CrudTest.Application.Queries/QueryApplicationAssembly.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Mc2.CrudTest.Application.Queries;
+
+public class QueryApplicationAssembly
+{
+    public static Assembly Assembly => Assembly.GetExecutingAssembly();
+}
