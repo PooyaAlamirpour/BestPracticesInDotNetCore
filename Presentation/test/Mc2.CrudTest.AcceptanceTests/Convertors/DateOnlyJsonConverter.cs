5e57afcfe1a8085426fb4a13d5b5485d4b7b01ec
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:20:07 +0330

    test: extensions and convertors are added

---
+++ Presentation/test/Mc2.CrudTest.AcceptanceTests/Convertors/DateOnlyJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Mc2.CrudTest.AcceptanceTests.Convertors;
+
+public class DateOnlyJsonConverter : JsonConverter<DateOnly>
+{
+    private const string Format = "M/d/yyyy";
+
+    public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateOnly.ParseExact(reader.GetString(), Format, CultureInfo.InvariantCulture);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(Format, CultureInfo.InvariantCulture));
+    }
+}
---
+++ Presentation/test/Mc2.CrudTest.AcceptanceTests/Extensions/JsonExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Mc2.CrudTest.AcceptanceTests.Extensions;
+
+public static class JsonExtensions
+{
+    public static string ParseJson(this string json)
+    {
+        using var jDoc = JsonDocument.Parse(json);
+        return JsonSerializer.Serialize(jDoc, new JsonSerializerOptions { WriteIndented = true });
+    }
+}
