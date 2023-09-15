57a405fd8f5deda723c40b4b67c50805e71954d9
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:45:08 +0330

    feat: event bust is added for dispatching events

---
+++ Infrastructure/src/Mc2.CrudTest.Infrastructure.Persistence/EventBus/EventDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using Mc2.CrudTest.framework.Mediator.Abstracts;
+using MediatR;
+
+namespace Mc2.CrudTest.Infrastructure.Persistence.EventBus;
+
+public class EventDispatcher : IEventDispatcher
+{
+    private readonly IPublisher _eventPublisher;
+
+    public EventDispatcher(IPublisher eventPublisher)
+    {
+        _eventPublisher = eventPublisher;
+    }
+
+    public async Task DispatchAsync(IDomainEvent @event, CancellationToken cancellationToken = default)
+    {
+        await _eventPublisher.Publish(@event, cancellationToken);
+    }
+}
