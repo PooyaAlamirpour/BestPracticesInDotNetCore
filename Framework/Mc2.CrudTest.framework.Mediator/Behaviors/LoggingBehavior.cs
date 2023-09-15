3cf4554632b54c70ace552dd249d59f86b238ace
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:46:14 +0330

    feat: mediatR behavioral is added

---
+++ Framework/Mc2.CrudTest.framework.Mediator/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using ErrorOr;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Mc2.CrudTest.framework.Mediator.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : IErrorOr
+{
+    private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await next();
+            if (result.IsError)
+            {
+                _logger.LogError($"{typeof(TRequest).Name} with " +
+                                 $"{JsonSerializer.Serialize(request)} " +
+                                 " is failed. Errors: " +
+                                 $"{JsonSerializer.Serialize(result.Errors)}");
+            }
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"{typeof(TRequest).Name} with " +
+                             $"{JsonSerializer.Serialize(request)} " +
+                             " is failed. Errors: " +
+                             $"{JsonSerializer.Serialize(ex)}");
+            throw;
+        }
+        
+    }
+}
---
+++ Framework/Mc2.CrudTest.framework.Mediator/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace Mc2.CrudTest.framework.Mediator.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    private readonly IValidator<TRequest>? _validator;
+
+    public ValidationBehavior(IValidator<TRequest>? validator)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken)
+    {
+        if (_validator is null)
+        {
+            return await next();
+        }
+
+        var validatorResult = await _validator.ValidateAsync(request, cancellationToken);
+        if (validatorResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validatorResult.Errors.ConvertAll(validationFailure =>
+            Error.Validation(validationFailure.ErrorCode, validationFailure.ErrorMessage));
+
+        return (dynamic)errors;
+    }
+}
