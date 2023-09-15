5519ac7fd99182368b661269fba97c27902a731f
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:23:14 +0330

    feat: base conteoller is added

---
+++ Presentation/src/Mc2.CrudTest.Presentation/Controllers/Base/ApiBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mc2.CrudTest.Presentation.Server.Controllers.Base;
+
+[ApiController]
+[Route("api/v{version:apiVersion}")]
+public class ApiBase : ControllerBase
+{
+    
+}
---
+++ Presentation/src/Mc2.CrudTest.Presentation/Controllers/Base/ApiRoutes.cs
@@ -0,0 +1,12 @@
+﻿namespace Mc2.CrudTest.Presentation.Server.Controllers.Base;
+
+public static partial class ApiRoutes
+{
+    public static class Customer
+    {
+        public const string Get = "customer";
+        public const string Create = "customer";
+        public const string Update = "customer/{customerId:guid}";
+        public const string Delete = "customer/{customerId:guid}";
+    }
+}
