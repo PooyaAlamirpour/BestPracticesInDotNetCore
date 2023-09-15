9b565fcaa31cb829aa99905fa96ac0e0a95fc0c6
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:35:28 +0330

    test: test bank account number rule

---
+++ Domain/test/Mc2.CrudTest.Domain.Core.UnitTests/BankAccountNumberMustBeValidRuleTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Mc2.CrudTest.Domain.Core.Customer.Rules;
+using Mc2.CrudTest.framework.DDD.Abstracts;
+
+namespace Mc2.CrudTest.Domain.Core.Tests;
+
+public class BankAccountNumberMustBeValidRuleTests
+{
+    private IBusinessRule _sut;
+    
+    // [MethodName]_Should_[Action]_When_[Condition]
+
+    [Fact]
+    public void HasValidRule_Should_ReturnsTrue_When_GivenValidBankAccountNumber()
+    {
+        // Assign
+        var bankAccountNumber = "6104000000001111";
+        _sut = new BankAccountNumberMustBeValidRule(bankAccountNumber);
+        
+        // Act
+        var response = _sut.HasValidRule();
+
+        // Assert
+        response.Should().BeTrue();
+    }
+    
+    [Theory]
+    [InlineData("6104-0000-0000-1111")]
+    [InlineData("61040000000011110")]
+    [InlineData("123456")]
+    public void HasValidRule_Should_ReturnsFalse_When_GivenInvalidBankAccountNumber(string bankAccountNumber)
+    {
+        // Assign
+        _sut = new BankAccountNumberMustBeValidRule(bankAccountNumber);
+        
+        // Act
+        var response = _sut.HasValidRule();
+
+        // Assert
+        response.Should().BeFalse();
+    }
+}
