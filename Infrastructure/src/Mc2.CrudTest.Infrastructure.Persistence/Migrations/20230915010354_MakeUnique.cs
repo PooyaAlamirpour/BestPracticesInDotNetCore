a5b8a4e7f87446ab9f76b084bc094d908a99eb47
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:44:19 +0330

    feat: make unique firstname, lastname, birthday and email. Index is defined for those

---
+++ Infrastructure/src/Mc2.CrudTest.Infrastructure.Persistence/Migrations/20230915010354_MakeUnique.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mc2.CrudTest.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "customers",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customers_email",
+                table: "customers",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customers_first_name_last_name_date_Of_birth",
+                table: "customers",
+                columns: new[] { "first_name", "last_name", "date_Of_birth" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_customers_email",
+                table: "customers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_customers_first_name_last_name_date_Of_birth",
+                table: "customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "customers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
