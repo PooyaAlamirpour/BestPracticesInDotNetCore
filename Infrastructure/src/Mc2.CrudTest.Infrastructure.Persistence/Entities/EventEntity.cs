8e884e191981a8352578eb46ef486fb9571a1c40
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:42:03 +0330

    feat: event entity is added

---
+++ Infrastructure/src/Mc2.CrudTest.Infrastructure.Persistence/Entities/EventEntity.cs
@@ -0,0 +1,14 @@
+﻿using Mc2.CrudTest.framework.DDD;
+using Mc2.CrudTest.framework.DDD.Abstracts;
+
+namespace Mc2.CrudTest.Infrastructure.Persistence.Entities;
+
+public class EventEntity : Entity<Guid>, IAuditable
+{
+    public Guid AggregateId { get; set; }
+    public string AggregateType { get; set; }
+    public string Payload { get; set; } = string.Empty;
+    public long Version { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+}
