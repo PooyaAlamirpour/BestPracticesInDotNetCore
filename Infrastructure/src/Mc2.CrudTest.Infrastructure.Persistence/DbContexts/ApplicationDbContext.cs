ad45fb5a2bc9c331756cc2c354e2b41062f3d1a6
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:41:43 +0330

    feat: db context is defined. savechange is overirded

---
+++ Infrastructure/src/Mc2.CrudTest.Infrastructure.Persistence/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Mc2.CrudTest.framework.DDD.Abstracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Mc2.CrudTest.Infrastructure.Persistence.DbContexts;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        foreach (var property in entity.GetProperties().Where(p => p.IsPrimaryKey()))
+            property.ValueGenerated = ValueGenerated.Never;
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.LogTo(Console.WriteLine);
+    }
+
+    public override int SaveChanges()
+    {
+        UpdateAuditableEntity();
+
+        return base.SaveChanges();
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        UpdateAuditableEntity();
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+    
+    private void UpdateAuditableEntity()
+    {
+        foreach (var entityEntry in ChangeTracker.Entries<IAuditable>())
+        {
+            if (entityEntry.State == EntityState.Added)
+            {
+                entityEntry.Property(entity => entity.CreatedAt).CurrentValue = DateTime.Now;
+            }
+
+            if (entityEntry.State == EntityState.Modified)
+            {
+                entityEntry.Property(entity => entity.ModifiedAt).CurrentValue = DateTime.Now;
+            }
+        }
+    }
+}
