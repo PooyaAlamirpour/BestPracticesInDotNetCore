4d8a3156493583c5785d0eaf4ef12068a87a9501
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:29:12 +0330

    feat: update command is implemented as a pipeline

---
+++ Application/src/Mc2.CrudTest.Application.Command/Customer/Update/UpdateCustomerCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Mc2.CrudTest.Application.Command.Customer.Update;
+
+public record UpdateCustomerCommand(
+    Guid CustomerId,
+    string PhoneNumber,
+    string BankAccountNumber) : IRequest;
---
+++ Application/src/Mc2.CrudTest.Application.Command/Customer/Update/UpdateCustomerCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Mc2.CrudTest.Domain.Core.Customer;
+using Mc2.CrudTest.Domain.Core.Customer.ValueObjects;
+using Mc2.CrudTest.framework.Mediator.Abstracts;
+
+namespace Mc2.CrudTest.Application.Command.Customer.Update;
+
+public class UpdateCustomerCommandHandler : ICommandHandler<UpdateCustomerCommand>
+{
+    private readonly IEventStoreRepository<CustomerAggregateRoot, CustomerId> _eventStoreRepository;
+
+    public UpdateCustomerCommandHandler(IEventStoreRepository<CustomerAggregateRoot, CustomerId> eventStoreRepository)
+    {
+        _eventStoreRepository = eventStoreRepository;
+    }
+
+    public async Task Handle(UpdateCustomerCommand message, CancellationToken cancellationToken)
+    {
+        CustomerAggregateRoot customer = await _eventStoreRepository.FetchStreamAsync(message.CustomerId, cancellationToken: cancellationToken);
+        customer.Update(message.CustomerId, message.PhoneNumber, message.BankAccountNumber);
+        await _eventStoreRepository.AppendEventsAsync(customer, cancellationToken);
+    }
+    
+}
