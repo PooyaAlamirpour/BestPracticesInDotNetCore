0c718001d52e7717d2cdf778ba7948683168443b
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:27:23 +0330

    style: command layer is created based on cqrs

---
+++ Application/src/Mc2.CrudTest.Application.Command/CommandApplicationAssembly.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Mc2.CrudTest.Application.Command;
+
+public static class CommandApplicationAssembly
+{
+    public static Assembly Assembly => Assembly.GetExecutingAssembly();
+}
---
+++ Application/src/Mc2.CrudTest.Application.Command/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using FluentValidation;
+using Mc2.CrudTest.Application.Command.Customer.Delete;
+using Mc2.CrudTest.framework.Mediator.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mc2.CrudTest.Application.Command;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCommandPipeline(this IServiceCollection services, Assembly assembly)
+    {
+        services.AddTransient<IValidator<DeleteCustomerCommand>, DeleteCustomerCommandValidator>();
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(assembly)
+                .AddOpenBehavior(typeof(ValidationBehavior<,>))
+                .AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        return services;
+    }
+}
