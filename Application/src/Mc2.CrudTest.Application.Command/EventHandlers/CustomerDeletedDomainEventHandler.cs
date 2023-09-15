144533542ab4f8ac4e87f94868b518c8e8f2ca9c
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:30:17 +0330

    feat: delete event handler is implemented

---
+++ Application/src/Mc2.CrudTest.Application.Command/EventHandlers/CustomerDeletedDomainEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using Mc2.CrudTest.Domain.Core.Customer;
+using Mc2.CrudTest.Domain.Core.Events;
+using Mc2.CrudTest.framework.Mediator.Abstracts;
+using Mc2.CrudTest.Infrastructure.Persistence.Repositories.Abstracts;
+
+namespace Mc2.CrudTest.Application.Command.EventHandlers;
+
+public class CustomerDeletedDomainEventHandler : ICommandEventHandler<CustomerDeletedDomainEvent>
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public CustomerDeletedDomainEventHandler(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public async Task Handle(CustomerDeletedDomainEvent @event, CancellationToken cancellationToken)
+    {
+        CustomerAggregateRoot customer = new();
+        customer.Apply(@event);
+        _customerRepository.Delete(customer);
+        await _customerRepository.CommitAsync(cancellationToken);
+    }
+}
