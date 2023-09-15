70f3a2a52f1c1bf9e22c0e36c367ace8b69977ed
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:39:54 +0330

    feat: business exception is added for domain

---
+++ Domain/src/Mc2.CrudTest.Domain.Core/Exceptions/CustomerNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Mc2.CrudTest.Domain.Core.Exceptions;
+
+public class CustomerNotFoundException : Exception
+{
+    public CustomerNotFoundException() 
+        : base($"The requested customer is not found")
+    {
+    }
+}
