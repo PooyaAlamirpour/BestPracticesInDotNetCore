2beb97518c9d9114f5cbc4cdc9f10c4dbbe95e07
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:42:28 +0330

    feat: fluent configuration is added for entities

---
+++ Infrastructure/src/Mc2.CrudTest.Infrastructure.Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,61 @@
+﻿using Mc2.CrudTest.Domain.Core.Customer;
+using Mc2.CrudTest.Domain.Core.Customer.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mc2.CrudTest.Infrastructure.Persistence.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<CustomerAggregateRoot>
+{
+    public void Configure(EntityTypeBuilder<CustomerAggregateRoot> entity)
+    {
+        entity.ToTable("customers");
+        
+        entity.HasKey(x => x.Id).HasName("PRIMARY");
+        entity.Property(x => x.Id)
+            .HasConversion(paymentId => paymentId.Value,
+                value => CustomerId.Of(value))
+            .HasColumnName("id");
+
+        entity.Property(x => x.Firstname)
+            .HasMaxLength(50)
+            .HasColumnName("first_name");
+        
+        entity.Property(x => x.Lastname)
+            .HasMaxLength(50)
+            .HasColumnName("last_name");
+        
+        entity.Property(x => x.DateOfBirth)
+            .HasColumnName("date_Of_birth")
+            .HasMaxLength(50)
+            .HasConversion(birthdate => birthdate.Value,
+                value => DateOfBirthdate.Of(value.ToString()));
+       
+        entity.Property(x => x.PhoneNumber)
+            .HasColumnName("phone_number")
+            .HasMaxLength(15)
+            .HasConversion(phoneNumber => phoneNumber.Value,
+                value => PhoneNumber.Of(value.ToString()));
+
+        entity.Property(x => x.Email)
+            .HasConversion(email => email.Value,
+                value => Email.Of(value.ToString()))
+            .HasColumnName("email");
+
+        entity.Property(x => x.BankAccountNumber)
+            .HasConversion(bankAccountNumber => bankAccountNumber.Value,
+                value => BankAccountNumber.Of(value.ToString()))
+            .HasColumnName("bank_account_number");
+
+        entity.Ignore(x => x.Version);
+
+        entity.HasIndex(x => new { x.Firstname, x.Lastname, x.DateOfBirth })
+            .IsUnique();
+
+        entity.HasIndex(x => x.Email)
+            .IsUnique();
+        
+        entity.Property(x => x.CreatedAt).HasColumnName("created_at");
+        entity.Property(x => x.ModifiedAt).HasColumnName("modified_at").IsRequired(false);
+    }
+}
---
+++ Infrastructure/src/Mc2.CrudTest.Infrastructure.Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Mc2.CrudTest.Infrastructure.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mc2.CrudTest.Infrastructure.Persistence.Configurations;
+
+public class EventConfiguration : IEntityTypeConfiguration<EventEntity>
+{
+    public void Configure(EntityTypeBuilder<EventEntity> entity)
+    {
+        entity.ToTable("events");
+
+        entity.HasKey(x => x.Id).HasName("PRIMARY");
+        entity.Property(x => x.Id).HasColumnName("id");
+        entity.Property(x => x.Payload).HasColumnName("payload");
+        entity.Property(x => x.Version).HasColumnName("version");
+        entity.Property(x => x.CreatedAt).HasColumnName("created_at");
+        entity.Property(x => x.ModifiedAt).HasColumnName("modified_at").IsRequired(false);
+    }
+}
