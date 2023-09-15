fd80722e15fa8c07cde588c9b362019d75b33b7c
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:48:17 +0330

    feat: an extension for showing some errors is added to the fluent validation

---
+++ Framework/Mc2.CrudTest.framework.Mediator/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ErrorOr;
+using FluentValidation;
+
+namespace Mc2.CrudTest.framework.Mediator.Extensions;
+
+public static class FluentValidationExtensions
+{
+    public static IRuleBuilderOptions<T, TProperty> WithError<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule,
+        Error error) => rule
+        .WithErrorCode(error.Code)
+        .WithMessage(error.Description);
+}
