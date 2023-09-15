606e377988caa37a5da4225c02b0f2c845d2752a
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-15 06:47:30 +0330

    feat: generic logi is added

---
+++ Framework/Mc2.CrudTest.framework.domain/Logics/GenericLogic.cs
@@ -0,0 +1,15 @@
+﻿using Mc2.CrudTest.framework.DDD.Abstracts;
+using Mc2.CrudTest.framework.DDD.Exceptions;
+
+namespace Mc2.CrudTest.framework.DDD.Logics;
+
+public abstract class GenericLogic
+{
+    protected static void CheckRule(IBusinessRule rule)
+    {
+        if (!rule.HasValidRule())
+        {
+            throw new BusinessRuleValidationException(rule);
+        }
+    }
+}
