3ba6da8b813e589682e0457e4271af5d20fd3d48
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-21 03:56:57 +0330

    style: cleaning program.cs

--- Presentation/src/Mc2.CrudTest.Presentation/Program.cs
+++ Presentation/src/Mc2.CrudTest.Presentation/Program.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using Mc2.CrudTest.Application.Command;
 using Mc2.CrudTest.Application.Queries;
 using Mc2.CrudTest.Infrastructure.EventStore;
@@ -17,45 +16,48 @@ namespace Mc2.CrudTest.Presentation.Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            {
+                builder.Services.AddPresentation();
+                builder.Services.AddCommandPipeline(CommandApplicationAssembly.Assembly);
+                builder.Services.AddQueryPipeline(QueryApplicationAssembly.Assembly);
+                builder.Services.AddReadInfrastructure(builder.Configuration);
+                builder.Services.AddWriteInfrastructure(builder.Configuration);
+                builder.Services.AddEventStore(builder.Configuration);
 
-            builder.Services.AddPresentation();
-            builder.Services.AddCommandPipeline(CommandApplicationAssembly.Assembly);
-            builder.Services.AddQueryPipeline(QueryApplicationAssembly.Assembly);
-            builder.Services.AddReadInfrastructure(builder.Configuration);
-            builder.Services.AddWriteInfrastructure(builder.Configuration);
-            builder.Services.AddEventStore(builder.Configuration);
-
-            builder.Services.AddSwaggerGen();
+                builder.Services.AddSwaggerGen();
+            }
             
             var app = builder.Build();
-            
-            app.UseMiddleware<ExceptionHandlingMiddleware>();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
             {
-                var apiVersionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
-                app.UseSwagger();
-                app.UseSwaggerUI(options =>
+                app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
                 {
-                    foreach (string groupName in apiVersionProvider.ApiVersionDescriptions.Select(x => x.GroupName))
+                    var apiVersionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+                    app.UseSwagger();
+                    app.UseSwaggerUI(options =>
                     {
-                        options.SwaggerEndpoint($"/swagger/{groupName}/swagger.json", groupName.ToUpperInvariant());
-                    }
-                });
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+                        foreach (string groupName in apiVersionProvider.ApiVersionDescriptions.Select(x => x.GroupName))
+                        {
+                            options.SwaggerEndpoint($"/swagger/{groupName}/swagger.json", groupName.ToUpperInvariant());
+                        }
+                    });
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
 
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.MapControllers();
+                app.UseHttpsRedirection();
+                app.UseRouting();
+                app.MapControllers();
 
-            app.Run();
+                app.Run();
+            }
+            
         }
     }
 }
