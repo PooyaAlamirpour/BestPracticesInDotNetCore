88c78fe78fdfe29b98e67565c472d763ffd27e8f
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-25 02:13:09 +0330

    refact: value object is updated

--- Framework/src/BestPracticeInDotNet.framework.domain/ValueObject.cs
+++ Framework/src/BestPracticeInDotNet.framework.domain/ValueObject.cs
@@ -2,7 +2,35 @@
 
 namespace BestPracticeInDotNet.framework.DDD;
 
-public class ValueObject<TKey> : GenericLogic
+public abstract class ValueObject<TKey> : GenericLogic, IEquatable<ValueObject<TKey>>
 {
-    public Guid Value { get; set; }
+    public TKey Value { get; set; }
+    public abstract IEnumerable<object> GetEqualityComponents();
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((ValueObject<TKey>)obj);
+    }
+
+    public static bool operator ==(ValueObject<TKey> left, ValueObject<TKey> right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ValueObject<TKey> left, ValueObject<TKey> right)
+    {
+        return !(left == right);
+    }
+
+    public override int GetHashCode()
+    {
+        return EqualityComparer<TKey>.Default.GetHashCode(Value);
+    }
+
+    public bool Equals(ValueObject<TKey>? other)
+    {
+        return Equals((object?)other);
+    }
 }
