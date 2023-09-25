02255d9ed7ce43bade66b7aa89b73ff0b06d5475
Pooya Alamirpour <Pooya.Alamirpour@gmail.com>
2023-09-25 14:52:43 +0330

    fix: an error in saving data in datbase is resolved

--- Domain/src/BestPracticeInDotNet.Domain.Core/Customer/ValueObjects/CustomerId.cs
+++ Domain/src/BestPracticeInDotNet.Domain.Core/Customer/ValueObjects/CustomerId.cs
@@ -2,15 +2,15 @@
 
 namespace BestPracticeInDotNet.Domain.Core.Customer.ValueObjects;
 
-public class CustomerId : ValueObject<CustomerId>
+public class CustomerId : ValueObject<Guid>
 {
     private CustomerId(Guid customerId)
     {
-        // this.Value = customerId;
+        this.Value = customerId;
     }
 
     public static CustomerId Of(Guid customerId) => new(customerId);
-    public override IEnumerable<CustomerId> GetEqualityComponents()
+    public override IEnumerable<Guid> GetEqualityComponents()
     {
         throw new NotImplementedException();
     }
